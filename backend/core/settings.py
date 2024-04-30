73246b81f0e9b714f9d8fda0877d2d08be5fe58c
Jubril Tayo <78751604+jubriltayo@users.noreply.github.com>
2024-04-30 10:10:13 +0100

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -101,15 +101,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'taskdb',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+# }
+
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'taskdb',
-       'USER': 'postgres',
-       'PASSWORD': 'postgres',
-       'HOST': '127.0.0.1',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'jFfhIRMOBMGJrFeoEkytwdLxfIPJlGtO',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '47912',
+    }
 }
 
 AUTH_USER_MODEL = "users.User"
