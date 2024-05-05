ca0f786fc03243dbd4e32c1d36a481b4c480bbf7
Tayo <jubriltayo@gmail.com>
2024-05-05 22:39:56 +0100

    test2

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,18 +17,33 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import permissions
+from django.conf.urls.static import static
+from rest_framework_swagger.views import get_swagger_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
+
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="Tasks API",
+#       default_version='v1',
+#       description="API Documentation of a Task app",
+#    ),
+#    public=True,
+#    permission_classes=(permissions.AllowAny,),
+# )
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Tasks API",
-      default_version='v1',
-      description="API Documentation of a Task app",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Episyche Technologies",
+        default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
@@ -38,5 +53,9 @@ urlpatterns = [
     path('api/tasks/', include('tasks.urls')),
     path('api/auth/', include('users.urls')),
 
-    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
+    # path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
