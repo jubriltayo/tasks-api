119de73c7fc862083a6c40b275b1c839f8420fe4
Tayo <jubriltayo@gmail.com>
2024-05-08 00:06:10 +0100

    swagger updated

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -24,35 +24,22 @@ from drf_yasg import openapi
 from rest_framework import permissions
 from django.conf import settings
 
-# from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
-
-# schema_view = get_schema_view(
-#    openapi.Info(
-#       title="Tasks API",
-#       default_version='v1',
-#       description="API Documentation of a Task app",
-#    ),
-#    public=True,
-#    permission_classes=(permissions.AllowAny,),
-# )
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Episyche Technologies",
-        default_version='v1',),
+        title="Tasks API",
+        default_version='v1',
+        description="API Documentation of a Task app",
+    ),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/tasks/', include('tasks.urls')),
     path('api/auth/', include('users.urls')),
-
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
 ]
 
